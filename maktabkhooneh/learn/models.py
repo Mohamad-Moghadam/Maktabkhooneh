df213b9b9ba3a7612f75e43ac406ad45b4578301
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-10 21:56:00 +0330

    ✨ adding the database of programming course

--- maktabkhooneh/learn/models.py
+++ maktabkhooneh/learn/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Programming_Courses(models.Model):
+    name = models.CharField(max_length = 100)
+    teacher = models.ForeignKey(to = Teachers, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    number_of_students = models.IntegerField()
+    sessions = models.IntegerField()
+    rating = models.IntegerField()
+    hours = models.IntegerField()
+
