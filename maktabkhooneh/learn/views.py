f88b66fd3f01466ece84966a87dfde4e6dc66f27
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-11 21:53:26 +0330

    🚀 a better message is shown for each learn category

--- maktabkhooneh/learn/views.py
+++ maktabkhooneh/learn/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse, JsonResponse
 from learn.models import Programming_Courses
 
 def courses(request, course_type: str):
-    return HttpResponse(f"{course_type}")
+    return HttpResponse(f"This page is dedicated to: {course_type}")
 
