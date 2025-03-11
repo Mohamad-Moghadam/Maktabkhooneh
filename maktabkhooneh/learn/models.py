38a8683514ce76d82dbb188132cf203b70a62fe4
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-11 22:12:13 +0330

    ✨ adding model for the teachers

--- maktabkhooneh/learn/models.py
+++ maktabkhooneh/learn/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-class Programming_Courses(models.Model):
+class Courses(models.Model):
     name = models.CharField(max_length = 100)
-    #teacher = models.ForeignKey(to = Teachers, on_delete=models.CASCADE)
+    teacher = models.ManyToManyField('learn.Teacher', verbose_name = ("teachers"))
     price = models.IntegerField()
     number_of_students = models.IntegerField()
     sessions = models.IntegerField()
     rating = models.IntegerField()
     hours = models.IntegerField()
+
+class Teacher(models.Model):
+    name = models.CharField(max_length = 100)
+    discription = models.TextField()
+    number_of_students = models.IntegerField()
 
