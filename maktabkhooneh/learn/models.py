eebe41b1669b66f903095e241f390c47e779abaf
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-11 22:17:48 +0330

    ✨ adding model for the students

--- maktabkhooneh/learn/models.py
+++ maktabkhooneh/learn/models.py
@@ -14,3 +14,11 @@ class Teacher(models.Model):
     discription = models.TextField()
     number_of_students = models.IntegerField()
 
+class Student(models.Model):
+    name = models.CharField(max_length = 100)
+    maktabplus_sub = models.BooleanField(default = False)
+    courses = models.ForeignKey(to = Courses, on_delete = models.CASCADE, related_name = "courses of the students")
+    certifications = models.TextField()
+    asked_questions = models.TextField()
+    payments = models.TextField()
+    cart = models.ForeignKey(to = Courses, on_delete = models.CASCADE, related_name = "purchased_courses")
