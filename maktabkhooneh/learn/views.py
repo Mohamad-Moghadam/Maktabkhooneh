d1a3d8c41e3035fdfc495dbe4db563541721078b
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-11 21:51:12 +0330

    🐛 return bug fixed in view

--- maktabkhooneh/learn/views.py
+++ maktabkhooneh/learn/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse, JsonResponse
 from learn.models import Programming_Courses
 
 def courses(request, course_type: str):
-    HttpResponse(f"{course_type}")
+    return HttpResponse(f"{course_type}")
 
